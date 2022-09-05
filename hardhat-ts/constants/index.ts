2dc6dd73618fca39c587ec65d79e336995bc8c96
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2022-09-06 00:25:30 +0500

    first commit

---
+++ hardhat-ts/constants/index.ts
@@ -0,0 +1,5 @@
+// Address of the Whitelist Contract that you deployed
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x7f4ab19ff8c0822bcc22dba61e8d89102bd9611e";
+// URL to extract Metadata for a Crypto Dev NFT
+export const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
